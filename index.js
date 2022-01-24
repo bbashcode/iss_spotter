79c807f7d3dbf4105577bc5465296008a6513059
bbashcode <binbash.code@gmail.com>
2022-01-24 04:32:04 +0000

    ISS Spotter Part III: Fetch flyover times for iss is working as expected

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // index.js
 
-// The code below is temporary and can be commented out.
-const { fetchCoordsByIP } = require('./iss');
 
-fetchCoordsByIP('67.70.57.95', (error, coordinates) => {
+// The code below is temporary and can be commented out.
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
--- iss.js
+++ iss.js
@@ -12,24 +12,24 @@
 
  const request = require('request');
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
 
- const fetchCoordsByIP = function(ip, callback) {
-  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+  request(url, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
 
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
       return;
     }
 
-    const { latitude, longitude } = JSON.parse(body);
-
-    callback(null, { latitude, longitude });
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
   });
 };
 
-// Don't need to export the other function since we are not testing it right now.
-module.exports = { fetchCoordsByIP };
+// Don't need to export the other functions since we are not testing them right now.
+module.exports = { fetchISSFlyOverTimes };
